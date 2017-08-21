628b3b751fc22eb458990d9525b42f06c3db095e
liuyupeng <liuyupeng@hey900.com>
2017-08-21 10:54:38 +0800

    新建main

---
+++ opengoapp/src/main/java/com/opengo/opengoapp/Main.java
@@ -0,0 +1,10 @@
+package com.opengo.opengoapp;
+
+/**
+ * Created by itservice on 2017/8/21.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ opengoapp/src/main/java/com/opengo/opengoapp/frame/MainFrame.java
@@ -0,0 +1,7 @@
+package com.opengo.opengoapp.frame;
+
+/**
+ * Created by itservice on 2017/8/21.
+ */
+public class MainFrame {
+}
